0cbe64c1fb8dcdb03abd9cd811a70b09ebaef02f
Juan Boza <Juanboza80@gmail.com>
2021-02-03 19:03:22 -0700

    local storage path created for textarea class, needs button click function

--- javascript.js
+++ javascript.js
@@ -13,6 +13,16 @@ function startTime() {
 }
 // current date 
 
+localStorageFunction();
 
-
-
+function localStorageFunction() {
+    $("textarea")[0].value = localStorage.getItem("textarea1");
+    $("textarea")[1].value = localStorage.getItem("textarea2");
+    $("textarea")[2].value = localStorage.getItem("textarea3");
+    $("textarea")[3].value = localStorage.getItem("textarea4");
+    $("textarea")[4].value = localStorage.getItem("textarea4");
+    $("textarea")[5].value = localStorage.getItem("textarea6");
+    $("textarea")[6].value = localStorage.getItem("textarea7");
+    $("textarea")[7].value = localStorage.getItem("textarea8");
+    $("textarea")[8].value = localStorage.getItem("textarea9");
+} 
